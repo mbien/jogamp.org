cb634f6aeec00ce36c42037a6fdc803dd58dffe8
Michael Bien <mbien@fh-landshut.de>
2010-08-23 20:06:15 +0200

    fixed file paths, added update-planet script.

---
+++ planet2/update-planet.sh
@@ -0,0 +1,3 @@
+java -jar HungryHarry-SNAPSHOT-jar-with-dependencies.jar /srv/www/jogamp.org/planet2/config/planet-config.xml
+mv ../planet/planet.html ../planet/index.html
+
--- planet2/update-stream.sh
+++ planet2/update-stream.sh
@@ -1,3 +1,3 @@
-java -jar HungryHarry-SNAPSHOT-jar-with-dependencies.jar /srv/www/jogamp.org/planet2/config/config.xml
+java -jar HungryHarry-SNAPSHOT-jar-with-dependencies.jar /srv/www/jogamp.org/planet2/config/stream-config.xml
 mv ../stream/planet.html ../stream/index.html
 
